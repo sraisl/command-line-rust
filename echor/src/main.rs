5c282d8fa41403c6a68edb5da1a8b2ce907c1835
Stefan Raisl <stefan.raisl@gmail.com>
2026-02-14 21:58:00 +0100

    initial commit echo tool

---
+++ echor/src/main.rs
@@ -0,0 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+
+struct Args {
+    /// The string to echo
+    #[arg()]
+    input: String,
+    
+    #[arg(short = 'n', help = "Do not print the trailing newline")]
+    no_newline: bool,
+
+
+    
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if args.no_newline {
+        print!("{}", args.input);
+    } else {
+        println!("{}", args.input);
+    }
+}
